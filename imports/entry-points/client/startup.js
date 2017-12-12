9eec17652b09242a9eddb6a526eff0d7c321a56e
fede-rodes <rodesf@gmail.com>
2017-12-11 23:27:19 -0300

    rolling back client startup changes

--- imports/entry-points/client/startup.js
+++ imports/entry-points/client/startup.js
@@ -1,4 +1,51 @@
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+import { injectGlobal } from 'styled-components';
+import 'sanitize.css/sanitize.css';
+import 'basscss/css/basscss.min.css';
+import App from '../../ui/app';
+import theme from '../../ui/theme';
+
+Meteor.startup(() => {
+  const renderStart = Date.now();
+  const startupTime = renderStart - window.performance.timing.responseStart;
+  console.log(`Meteor.startup took: ${startupTime}ms`);
+
+  // Register service worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(() => console.info('Service worker is registered!'))
+    .catch(err => console.info('ServiceWorker registration failed: ', err));
+  }
+
+  // Inject react app
+  render(<App />, document.getElementById('root'));
+
+  const renderTime = Date.now() - renderStart;
+  console.log(`renderAsync took: ${renderTime}ms`);
+  console.log(`Total time: ${startupTime + renderTime}ms`);
+
+  // Global style
+  injectGlobal([`
+    html, body, #root {
+      margin: 0;
+      padding: 0;
+      font-family: 'Lato', 'Helvetica Neue', Arial, Helvetica, sans-serif;
+      background-image: linear-gradient(140deg, rgb(12,6,50) 20%, rgb(66,59,90) 60%, rgb(219,159,159) 100%);
+      min-height: 100vh;
+      font-size: ${theme.fontSize.normal};
+    }
+
+    a { color: ${theme.color.link}; }
+  `]);
+
+  const styleTime = Date.now() - renderStart;
+  console.log(`styleAsync took: ${styleTime}ms`);
+  console.log(`Total time: ${startupTime + styleTime}ms`);
+});
+
+/* import { Meteor } from 'meteor/meteor';
 
 // See: https://youtu.be/j-WcyAjVceM
 async function renderAsync() {
@@ -45,53 +92,4 @@ Meteor.startup(() => {
     console.log(`renderAsync took: ${renderTime}ms`);
     console.log(`Total time: ${startupTime + renderTime}ms`);
   });
-});
-
-/*
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { render } from 'react-dom';
-import { injectGlobal } from 'styled-components';
-import 'sanitize.css/sanitize.css';
-import 'basscss/css/basscss.min.css';
-import App from '../../ui/app';
-import theme from '../../ui/theme';
-
-Meteor.startup(() => {
-  const renderStart = Date.now();
-  const startupTime = renderStart - window.performance.timing.responseStart;
-  console.log(`Meteor.startup took: ${startupTime}ms`);
-
-  // Register service worker
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(() => console.info('Service worker is registered!'))
-    .catch(err => console.info('ServiceWorker registration failed: ', err));
-  }
-
-  // Inject react app
-  render(<App />, document.getElementById('root'));
-
-  const renderTime = Date.now() - renderStart;
-  console.log(`renderAsync took: ${renderTime}ms`);
-  console.log(`Total time: ${startupTime + renderTime}ms`);
-
-  // Global style
-  injectGlobal([`
-    html, body, #root {
-      margin: 0;
-      padding: 0;
-      font-family: 'Lato', 'Helvetica Neue', Arial, Helvetica, sans-serif;
-      background-image: linear-gradient(140deg, rgb(12,6,50) 20%, rgb(66,59,90) 60%, rgb(219,159,159) 100%);
-      min-height: 100vh;
-      font-size: ${theme.fontSize.normal};
-    }
-
-    a { color: ${theme.color.link}; }
-  `]);
-
-  const styleTime = Date.now() - renderStart;
-  console.log(`styleAsync took: ${styleTime}ms`);
-  console.log(`Total time: ${startupTime + styleTime}ms`);
-});
-*/
+}); */
