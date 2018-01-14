3d20369b5d2675976b8bec6b13b9dc5a4660ff2c
fede-rodes <rodesf@gmail.com>
2018-01-14 14:54:15 -0300

    update sw from VeliovGroup/Meteor-Files-Demos

--- public/sw.js
+++ public/sw.js
@@ -1,96 +1,59 @@
-const HTMLToCache = '/';
-const version = 'MSW V0.3';
+/* eslint-disable */
+// From https://github.com/VeliovGroup/Meteor-Files-Demos/blob/master/demo/public/sw.js
 
-function removeHash(element) {
-  if (typeof element === 'string') return element.split('?hash=')[0];
-  throw new Error(401, 'Element is not a string');
-}
+;(function (self) {
+  'use strict';
+  var CACHE_NAME = 'meteor_apollo_starter_v1';
+  var pages      = ['/', '/sw.min.js', '/manifest-pwa.json'];
+  var origin     = self.location.origin;
+  var RE         = {
+    method: /GET/i,
+    static: /\.(?:png|jpe?g|css|js|gif|webm|webp|eot|svg|ttf|woff|woff2)(?:\?[a-zA-Z0-9-._~:/#\[\]@!$&'()*+,;=]*)?$|(?:fonts\.googleapis\.com|gstatic\.com)/i,
+    sockjs: /\/sockjs\//
+  };
 
-function hasHash(element) {
-  if (typeof element === 'string') return /\?hash=.*/.test(element);
-  throw new Error(401, 'Element is not a string');
-}
+  self.addEventListener('install', function (event) {
+    self.skipWaiting();
+    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+      cache.addAll(pages);
+    }));
+  });
 
-function hasSameHash(firstUrl, secondUrl) {
-  if (typeof firstUrl === 'string' && typeof secondUrl === 'string') {
-    return /\?hash=(.*)/.exec(firstUrl)[1] === /\?hash=(.*)/.exec(secondUrl)[1];
-  }
-  throw new Error(401, 'Url is not a string');
-}
+  self.addEventListener('fetch', function (event) {
+    self.clients.claim();
+    if (RE.method.test(event.request.method) && !RE.sockjs.test(event.request.url)) {
+      var req = event.request.clone();
+      var uri = event.request.url.replace(origin, '');
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(version)
-    .then(cache => cache.add(HTMLToCache).then(self.skipWaiting()))
-    .catch(error => console.error('Failed to cache', error)),
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys()
-    .then(cacheNames => Promise.all(cacheNames.map((cacheName) => {
-      if (version !== cacheName) return caches.delete(cacheName);
-    })))
-    .then(self.clients.claim()),
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  const requestToFetch = event.request.clone();
-  event.respondWith(
-    // We look if the request fits an element in the cache
-    caches.match(event.request.clone()).then((cached) => {
-      // We don't return cached HTML (except if fetch failed)
-      if (cached) {
-        const resourceType = cached.headers.get('content-type');
-        // We only return non css/js/html cached response e.g images
-        if (!hasHash(event.request.url) && !/text\/html/.test(resourceType)) {
-          return cached;
-        }
-        // If the CSS/JS didn't change since it's been cached, return the cached version
-        if (hasHash(event.request.url) && hasSameHash(event.request.url, cached.url)) {
-          return cached;
-        }
-      }
-      return fetch(requestToFetch).then((response) => {
-        const clonedResponse = response.clone();
-        const contentType = clonedResponse.headers.get('content-type');
-        if (!clonedResponse || clonedResponse.status !== 200 || clonedResponse.type !== 'basic'
-        || /\/sockjs\//.test(event.request.url)) {
-          return response;
-        }
-        if (/html/.test(contentType)) {
-          caches.open(version).then(cache => cache.put(HTMLToCache, clonedResponse));
-        } else {
-          // Delete old version of a file
-          if (hasHash(event.request.url)) {
-            caches.open(version).then(cache => cache.keys().then(keys => keys.forEach((asset) => {
-              if (new RegExp(removeHash(event.request.url)).test(removeHash(asset.url))) {
-                cache.delete(asset);
-              }
-            })));
-          }
-          // Added by fede-rodes
-          if (event.request.method === 'GET') {
-            caches.open(version).then(cache => cache.put(event.request, clonedResponse));
-          }
+      event.respondWith(fetch(req).then(function (response) {
+        if (!!~pages.indexOf(uri) || RE.static.test(event.request.url)) {
+          var resp = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(req, resp);
+          });
         }
         return response;
-      }).catch(() => {
-        if (hasHash(event.request.url)) return caches.match(event.request.url);
-        // If the request URL hasn't been served from cache and isn't sockjs we suppose it's HTML
-        else if (!/\/sockjs\//.test(event.request.url)) return caches.match(HTMLToCache);
-        // Only for sockjs
-        return new Response('No connection to the server', {
-          status: 503,
-          statusText: 'No connection to the server',
-          headers: new Headers({ 'Content-Type': 'text/plain' }),
+      }).catch(function () {
+        return caches.match(req).then(function (cached) {
+          return cached || caches.match('/').catch(function () {
+            return fetch(req);
+          });
+        }).catch(function () {
+          return caches.match('/').catch(function () {
+            return fetch(req);
+          });
         });
-      });
-    }),
-  );
-});
+      }));
+    }
+  });
 
-// Service worker created by Ilan Schemoul alias NitroBAY as a specific Service Worker for Meteor
-// Please see https://github.com/NitroBAY/meteor-service-worker for the official project source
+  self.addEventListener('activate', function (event) {
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (cacheName) {
+        return caches.delete(cacheName).catch(function () {
+          return;
+        });
+      }));
+    }));
+  });
+})(this);
