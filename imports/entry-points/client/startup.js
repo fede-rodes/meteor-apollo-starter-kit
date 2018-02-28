6185be77e82cbb81780e7abc83b781eccbbefa12
fede-rodes <rodesf@gmail.com>
2018-02-28 12:05:29 +0100

    dynamic imports on startup

--- imports/entry-points/client/startup.js
+++ imports/entry-points/client/startup.js
@@ -1,17 +1,40 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { render } from 'react-dom';
-import App from '../../ui/app';
-import Routes from '../../ui/routes';
-import Header from '../../ui/components/smart/header';
-import Menu from '../../ui/components/smart/menu';
 
-Meteor.startup(() => {
-  // Register service worker
-  import '../../ui/register-sw';
+// Source: https://youtu.be/j-WcyAjVceM
+async function renderAsync() {
+  const [
+    React,
+    { render },
+    { default: App },
+    { default: Header },
+    { default: Routes },
+    { default: Menu },
+  ] = await Promise.all([
+    import('react'),
+    import('react-dom'),
+    import('../../ui/app'),
+    import('../../ui/components/smart/header'),
+    import('../../ui/routes'),
+    import('../../ui/components/smart/menu'),
+  ]);
 
-  // Inject react app components for App Shell Architecture
+  // Inject react app components into App's Shell
   render(<App component={Header} />, document.getElementById('header'));
   render(<App component={Menu} />, document.getElementById('menu'));
   render(<App component={Routes} />, document.getElementById('main'));
+}
+
+Meteor.startup(() => {
+  const renderStart = Date.now();
+  const startupTime = renderStart - window.performance.timing.responseStart;
+  console.log(`Meteor.startup took: ${startupTime}ms`);
+
+  // Register service worker
+  import('../../ui/register-sw').then(() => {});
+
+  renderAsync().then(() => {
+    const renderTime = Date.now() - renderStart;
+    console.log(`renderAsync took: ${renderTime}ms`);
+    console.log(`Total time: ${startupTime + renderTime}ms`);
+  });
 });
