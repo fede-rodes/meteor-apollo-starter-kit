8c45003c2dcc7ef17f4579d4fa2373894d4d5b6c
fede-rodes <rodesf@gmail.com>
2018-04-03 15:28:48 +0200

    refactoring forgot password page

--- imports/ui/pages/auth/forgot-password-page.js
+++ imports/ui/pages/auth/forgot-password-page.js
@@ -1,4 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SEO from '../../components/smart/seo';
+import AuthPageProps from '../../render-props/auth-page-props';
+import AuthPageLayout from '../../layouts/auth-page';
+import { PasswordAuthViews } from '../../components/smart/auth';
+import Feedback from '../../components/dumb/feedback';
+
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
+const ForgotPasswordPage = () => [
+  <SEO
+    key="seo"
+    schema="AboutPage"
+    title="Log In Page"
+    description="A starting point for Meteor applications."
+    contentType="product"
+  />,
+  <AuthPageProps key="view">
+    {({
+      disabled,
+      errorMsg,
+      successMsg,
+      setSuccessMessage,
+      handleBefore,
+      handleClientError,
+      handleServerError,
+      handleSuccess,
+    }) => (
+      <AuthPageLayout
+        title="Forgot your Password?"
+        subtitle="
+          We&apos;ll send a link to your email to reset
+          <br />
+          your password and get you back on track.
+        "
+      >
+        <PasswordAuthViews
+          view="forgotPassword"
+          btnLabel="Send Link"
+          disabled={disabled}
+          onBeforeHook={handleBefore}
+          onClientErrorHook={handleClientError}
+          onServerErrorHook={handleServerError}
+          onSuccessHook={() => {
+            // Extend authPageProps.handleSuccess' default functionality
+            handleSuccess(() => {
+              // Show success message after action is completed
+              setSuccessMessage('A new email has been sent to your inbox!');
+            });
+          }}
+        />
+        <Feedback
+          loading={disabled}
+          errorMsg={errorMsg}
+          successMsg={successMsg}
+        />
+        <p className="center">
+          <Link to="/login">Log in</Link> | <Link to="/signup">Sign up</Link>
+        </p>
+      </AuthPageLayout>
+    )}
+  </AuthPageProps>,
+];
+
+export default ForgotPasswordPage;
+
+
+/*
+import React from 'react';
 import SEO from '../../components/smart/seo';
 import AuthPageProps from '../../render-props/auth-page-props';
 import AuthPageLayout from '../../layouts/auth-page';
@@ -49,3 +119,4 @@ const ForgotPasswordPage = () => (
 );
 
 export default ForgotPasswordPage;
+*/
